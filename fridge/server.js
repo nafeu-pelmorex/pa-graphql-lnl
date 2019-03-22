ac6c771d70ca1defc867d1135b70a101f3cc4892
Nafeu Nasir <nafeu.nasir@gmail.com>
2019-03-22 12:24:41 -0400

    Update mock data

--- fridge/server.js
+++ fridge/server.js
@@ -8,11 +8,26 @@ const server = http.Server(app);
 const inventory = [
   'dough',
   'tomato sauce',
+  'pepperoni',
+  'tomato',
   'cheese',
   'bread',
   'garlic',
   'butter',
   'pasta',
+  'bacon',
+  'eggs',
+  'chicken',
+  'potatoes',
+  'lemon juice',
+  'peppers',
+  'onions',
+  'beef',
+  'gravy',
+  'lettuce',
+  'mayonaise',
+  'dressing',
+  'olives',
 ];
 
 server.listen(process.env.PORT || 8001, () => {
--- recipes/server.py
+++ recipes/server.py
@@ -4,6 +4,38 @@ app = Flask(__name__)
 
 recipes = [
     {
+        'name': 'cheese pizza',
+        'ingredients': ['dough', 'tomato sauce', 'cheese'],
+        'instructions': [
+            'Form dough',
+            'Add tomato sauce',
+            'Add cheese',
+            'Bake in oven for 20 minutes'
+        ],
+    },
+    {
+        'name': 'vegetarian pizza',
+        'ingredients': ['dough', 'tomato sauce', 'cheese', 'peppers', 'onions', 'olives'],
+        'instructions': [
+            'Form dough',
+            'Add tomato sauce',
+            'Add cheese',
+            'Add vegetables',
+            'Bake in oven for 20 minutes'
+        ],
+    },
+    {
+        'name': 'meatlovers pizza',
+        'ingredients': ['dough', 'tomato sauce', 'cheese', 'bacon', 'chicken', 'pepperoni'],
+        'instructions': [
+            'Form dough',
+            'Add tomato sauce',
+            'Add cheese',
+            'Prepare and add meat',
+            'Bake in oven for 20 minutes'
+        ],
+    },
+    {
         'name': 'pizza',
         'ingredients': ['dough', 'tomato sauce', 'cheese'],
         'instructions': [
@@ -40,6 +72,61 @@ recipes = [
             'Bake in oven for 15 minutes',
         ],
     },
+    {
+        'name': 'carbanara',
+        'ingredients': ['eggs', 'bacon', 'pasta'],
+        'instructions': [
+            'Cook bacon',
+            'Boil pasta',
+            'Mix pasta with bacon',
+            'Mix with egg'
+        ]
+    },
+    {
+        'name': 'lemon chicken',
+        'ingredients': ['chicken', 'potatoes', 'lemon juice'],
+        'instructions': [
+            'Preheat oven',
+            'Prepare chicken and potatoes',
+            'Mix with lemon juice',
+            'Bake'
+        ]
+    },
+    {
+        'name': 'bacon and eggs',
+        'ingredients': ['bacon', 'eggs'],
+        'instructions': [
+            'Cook them in pan'
+        ]
+    },
+    {
+        'name': 'mac and cheese',
+        'ingredients': ['butter', 'cheese', 'pasta'],
+        'instructions': [
+            'Boil pasta',
+            'Cook in pan with cheese and butter'
+        ]
+    },
+    {
+        'name': 'poutine',
+        'ingredients': ['gravy', 'potatoes', 'cheese'],
+        'instructions': [
+            'Cut and prepare potatoes',
+            'Present heated with gravy and cheese',
+        ]
+    },
+    {
+        'name': 'blt sandwich',
+        'ingredients': ['bread', 'bacon', 'lettuce', 'tomato', 'mayonaise'],
+        'instructions': [
+            'Slice bread',
+            'Put bacon, lettuce, tomato and mayo in sandwich'
+        ]
+    },
+    {
+        'name': 'salad',
+        'ingredients': ['onions', 'lemon juice', 'salad dressing', 'lettuce', 'tomato', 'peppers']
+    }
 ]
 
 @app.route('/',  methods=['GET'])
